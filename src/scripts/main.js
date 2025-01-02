dcd42cb51e13bd80e2293a43eef6aeac9bf3fcdc
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2025-01-02 16:45:00 -0300

    chore:  image gallery developed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,21 @@
+import banner from "../assets/icons/hero/banner.svg";
 import elipse from "../assets/icons/hero/ellipse.svg";
 import patins from "../assets/icons/hero/patins.png";
 import play from "../assets/icons/hero/play.svg";
 import shop from "../assets/icons/hero/shopping-bag.svg";
 import stars from "../assets/icons/hero/stars-1.svg";
 import stars2 from "../assets/icons/hero/stars-2.svg";
+import image1 from "../assets/images/image-01.png";
+import image2 from "../assets/images/image-02.png";
+import image3 from "../assets/images/image-03.png";
+import image4 from "../assets/images/image-04.png";
+import person from "../assets/images/person.jpg";
 import "../styles/index.css";
 document.querySelector("#app").innerHTML = `
   <main>
   <nav>
       <a href="#">
-        <img src="../logo-sintap.svg" alt="Logo Sintap Patins" />
+        <img  src="../logo-sintap.svg" alt="Logo Sintap Patins" />
       </a>
       <a href="#">
         <img
@@ -49,9 +55,67 @@ document.querySelector("#app").innerHTML = `
       <img src=${patins} alt="patins"/>
       <img src=${stars} alt="estrelas 1" />
       <img src=${stars2} alt="estrelas 2" />
-      
+            </div>
       </div>
-      </div>
+    </section>
+    <section class="banner">
+  <div class="scroller bg-gradient">
+  <div class="rolling">
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+   <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+   <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+   <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  <img src=${banner} alt="item do banner" />
+  
+  </div>
+  </div>
+    </section>
+  <section class="gallery">
+  <header>
+  <span>GALERIA DE FOTOS</span>
+  <h2>#usesnitap por ai</h2>
+  </header>
+  <div class="content"> 
+  <figure>
+  <img src=${image1} alt="item da galeria" />
+  <figcaption>
+  <img width="32" height="32": src=${person} alt="imagem Perfil Emmanuel Marcos Oliveira" />
+  <span>@Emmanuel M Oliveira</span>
+  </figcaption>
+  </figure>
+  <figure data-delay>
+  <img src=${image2} alt="item da galeria" />
+  <figcaption>
+  <img width="32" height="32": src=${person} alt="imagem Perfil Emmanuel Marcos Oliveira" />
+  <span>@Emmanuel M Oliveira</span>
+  </figcaption>
+  </figure>
+  <figure>
+  <img src=${image3} alt="item da galeria" />
+  <figcaption>
+  <img width="32" height="32": src=${person} alt="imagem Perfil Emmanuel Marcos Oliveira" />
+  <span>@Emmanuel M Oliveira</span>
+  </figcaption>
+  </figure>
+  <figure data-delay>
+  <img src=${image4} alt="item da galeria" />
+  <figcaption>
+  <img width="32" height="32": src=${person} alt="imagem Perfil Emmanuel Marcos Oliveira" />
+  <span>@Emmanuel M Oliveira</span>
+  </figcaption>
+  </figure>
+  </div>
     </section>
    </main>
 `;
